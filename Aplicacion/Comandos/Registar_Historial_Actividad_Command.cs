9c42f85f80d0784abfa801eebde5d9e5220731c8
Juan <north3j@gmail.com>
2025-11-13 12:25:14 -0400

    Feat #45 : Registrar en Historial

---
+++ Aplicacion/Comandos/Registar_Historial_Actividad_Command.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aplicacion.DTO;
+using MediatR;
+
+namespace Aplicacion.Comandos
+{
+    public class Registar_Historial_Actividad_Command: IRequest<Guid>
+    {
+        public Registar_Historial_Actividad_Dto dto { get; set; }
+        public Registar_Historial_Actividad_Command(Registar_Historial_Actividad_Dto d) {dto = d; }
+    }
+}
