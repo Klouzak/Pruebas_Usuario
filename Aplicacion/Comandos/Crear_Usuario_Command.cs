b3de4799ff61d83b2b797d725cfbcb22333f346e
Juan <north3j@gmail.com>
2025-11-13 11:57:57 -0400

    Feat #44: Crear Usuario.

---
+++ Aplicacion/Comandos/Crear_Usuario_Command.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aplicacion.DTO;
+using MediatR;
+
+namespace Aplicacion.Comandos
+{
+    public class Crear_Usuario_Command : IRequest<Guid>
+    {
+        public Crear_Usuario_DTO dto { get; set; }
+        public Crear_Usuario_Command(Crear_Usuario_DTO dto) {this.dto = dto; }
+    }
+}
